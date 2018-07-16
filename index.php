6988ec5f57ce678f5ee130e0a4be8293a842f23b
Grant Hanrahan <grantjhanrahan@gmail.com>
2018-07-16 13:14:21 +1000

    index.php, register.php, login.php, mysql db setup

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP site</title>
+</head>
+<body>
+
+    <?php 
+        echo "<p>...</p>";
+    ?>
+    <a href="login.php">Click here to login
+    <a href="register.php">Click here to register    
+
+</body>
+</html>
